5171e784d0d4a4e32eab110d256b3a3591d820b4
nurullah namal <77736724+nurullahnamal@users.noreply.github.com>
2023-11-03 09:38:41 +0300

    Generic Repository

--- src/Api/Infrastructure/BlazorSozluk.Api.Infrastructure.Persistence/Extensions/Registration.cs
+++ src/Api/Infrastructure/BlazorSozluk.Api.Infrastructure.Persistence/Extensions/Registration.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BlazorSozluk.Api.Application.Interfaces.Repositories;
 using BlazorSozluk.Api.Infrastructure.Persistence.Context;
+using BlazorSozluk.Api.Infrastructure.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -24,9 +26,10 @@ namespace BlazorSozluk.Api.Infrastructure.Persistence.Extensions
                 });
             });
 
-            var seedData = new SeedData();
-            seedData.SeedAsync(configuration).GetAwaiter().GetResult();
-            
+            //var seedData = new SeedData();
+            //seedData.SeedAsync(configuration).GetAwaiter().GetResult();
+
+            services.AddScoped<IUserRepository, UserRepository>();
 
 
             return services;
---
+++ src/Api/Infrastructure/BlazorSozluk.Api.Infrastructure.Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlazorSozluk.Api.Application.Interfaces.Repositories;
+using BlazorSozluk.Api.Domain.Models;
+using BlazorSozluk.Api.Infrastructure.Persistence.Context;
+
+namespace BlazorSozluk.Api.Infrastructure.Persistence.Repositories
+{
+    public class UserRepository:GenericRepository<User>,IUserRepository
+    {
+        public UserRepository(BlazorSozlukContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
